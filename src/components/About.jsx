56c06bd106cf8bedba2e62b8318b30b6c2710f75
BurhanuddinRaja <mails4burhan@gmail.com>
2022-08-27 16:50:16 +0530

    Introduction and navbar design changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,11 +14,7 @@ const About = () => {
                             <p>This is about me Hello</p>
                         </div>
                         <div className="image">
-                            <div className="my-image relative inline-block after:content-[''] after:h-52 after:w-48 after:absolute after:border-cyan-400 after:border-2 after:top-2 after:left-4 after:rounded-xl">
-                                <div className="absolute top-0 left-0 h-52 w-48">
-                                    <img src="/assets/Burhan.jpg" alt="Burhanuddin Raja" className='h-52 w-48 rounded-xl' />
-                                </div>
-                            </div>
+                            <img src="/assets/Burhan.jpg" alt="Burhanuddin Raja" className='h-52 w-48 rounded-xl' />
                         </div>
                     </div>
                 </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,26 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 
 const Navbar = () => {
 
-    const navigationItems = [{name:'About', pathTo: '/#about'}, {name:'Projects', pathTo: '/#projects'}, {name:'Experience', pathTo: '/#experience'}, {name:'Contact', pathTo: '/#contact'}]
+    const navigationItems = [{ name: 'About', pathTo: '/#about' }, { name: 'Projects', pathTo: '/#projects' }, { name: 'Experience', pathTo: '/#experience' }, { name: 'Contact', pathTo: '/#contact' }]
 
     return (
-        <header className="text-gray-600 body-font bg-slate-900">
+        <motion.header className="text-gray-600 body-font bg-slate-900"
+            initial={{ opacity: 0.2}}
+            animate={{ opacity: 1}}
+            transition={{ duration: 0.9 }}>
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <span className="ml-3 text-2xl text-white font-myName">&lt; <span className='text-cyan-400'>B</span> &gt;</span>
                 </a>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center mr-5">
                     {navigationItems.map((navItem, index) => {
-                    return <a href={`/${navItem.pathTo}`} className="mr-5 hover:text-cyan-400 hover:cursor-pointer text-gray-100" key={index}><span className='text-cyan-400 font-introHello font-bold ml-2 mr-1'>{index+1}.</span>{navItem.name}</a>
+                        return <a href={`/${navItem.pathTo}`} className="mr-5 hover:text-cyan-400 hover:cursor-pointer text-gray-100" key={index}><span className='text-cyan-400 font-introHello font-bold ml-2 mr-1'>{index + 1}.</span>{navItem.name}</a>
                     })}
                 </nav>
             </div>
-        </header>
+        </motion.header>
     )
 }
 
