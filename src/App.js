f85e74fe4cde11c1daef29cdb4e75f66b4ee5f78
BurhanuddinRaja <mails4burhan@gmail.com>
2022-09-08 20:14:04 +0530

    loading screen designed

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Blogs from './components/Blogs';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Intorduction from './components/Intorduction';
+import Loading from './components/Loading';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 
 function App() {
   return (
-    <div className='App bg-gradient-to-b from-black to-[#000d4e] overflow-hidden'>
+    <div className='App bg-gradient-to-b from-black to-[#030a2c] overflow-hidden'>
       <ParallaxProvider>
         <Navbar />
         <main >
           {/* <ParallexIntro /> */}
+          <Loading />
           <Intorduction />
           <About />
           <Skills />
